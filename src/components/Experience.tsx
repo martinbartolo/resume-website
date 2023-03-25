7b508ce144aa72d69c0de2c6adeda0be2c34ce3c
Martin Bartolo <maba8956@colorado.edu>
2023-03-24 20:48:05 -0600

    Updated experience to new fiverr descriptions

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -20,14 +20,14 @@ export default function Experience({}: Props) {
             }}
             viewport={{ once: true }}
             transition={{ duration: 0.5 }}
-            className='absolute top-10 2xl:top-20 selectionwhite font-montserrat font-bold text-6xl lg:text-7xl 2xl:text-8xl'>
+            className='absolute top-10 2xl:top-20 selectionwhite font-montserrat font-bold text-6xl md:text-7xl 2xl:text-8xl'>
             Experience
         </motion.div>
 
         <motion.div 
             initial={{ y: 40 }}
             className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
-            <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[280px] md:w-[400px] lg:w-[501px] xl:w-[600px] 2xl:w-[800px] snap-center p-5 
+            <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[280px] md:w-[553px] lg:w-[553px] xl:w-[600px] 2xl:w-[800px] snap-center p-5 
             bg-black font-montserrat transition-opacity duration-150 overflow-hidden border border-white hover:border-2'>
                 <div className='px-1 2xl:p-5'>
                     <h4 className='text-lg md:text-2xl xl:text-3xl 2xl:text-5xl font-light selectionwhite'>Machine Learning Annotator</h4>
@@ -42,7 +42,7 @@ export default function Experience({}: Props) {
                 </div>
             </article>
 
-            <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[280px] md:w-[400px] lg:w-[501px] xl:w-[600px] 2xl:w-[800px] snap-center p-5 
+            <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[280px] md:w-[550px] lg:w-[550px] xl:w-[600px] 2xl:w-[800px] snap-center p-5 
             bg-black font-montserrat transition-opacity duration-150 overflow-hidden border border-white hover:border-2'>
                 <div className='px-1 2xl:p-5'>
                     <h4 className='text-lg md:text-2xl xl:text-3xl 2xl:text-5xl font-light selectionwhite'>Grader</h4>
@@ -55,16 +55,21 @@ export default function Experience({}: Props) {
                 </div>
             </article>
 
-            <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[280px] md:w-[401px] lg:w-[503px] xl:w-[600px] 2xl:w-[802px] snap-center p-5 
+            <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[280px] md:w-[550px] lg:w-[550px] xl:w-[600px] 2xl:w-[802px] snap-center p-5 
             bg-black font-montserrat transition-opacity duration-150 overflow-hidden border border-white hover:border-2'>
                 <div className='px-1 2xl:p-5'>
                     <h4 className='text-lg md:text-2xl xl:text-3xl 2xl:text-5xl font-light selectionwhite'>Freelance Graphic Designer</h4>
                     <p className='font-bold text-sm md:text-lg xl:text-lg 2xl:text-3xl py-1 2xl:py-3 selectionwhite'>Fiverr.com</p>
                     <p className='font-light text-sm md:text-base xl:text-md 2xl:text-lg py-1 selectionwhite'>May 2016 - July 2021</p>
                     <ul className='list-disc space-y-2 ml-5 2xl:py-3 text-xs md:text-base xl:text-md 2xl:text-xl text-left'>
-                        <li className='selectionwhite'>Grew a successful online store from the ground up, offering graphic design services 
-                            to more than 1000 individuals and brands.</li>
-                        <li className='selectionwhite'>Built a strong and loyal customer base, amassing over 1100 orders and 900 5-star reviews.</li>
+                        <li className='selectionwhite'>Grew a successful online store from the ground up on fiverr.com, offering graphic design services such as branding, logo design, 
+                        and social media graphics.</li>
+                        <li className='selectionwhite'>Worked with over 1000 individuals and brands such as 
+                            <a className='font-medium text-blue-600' href="https://www.principal-it.com/" target="_blank"> Principal IT</a> and 
+                            <a className='font-medium text-blue-600' href="https://barkbutter.au/" target="_blank"> Bark Butter</a>.</li>
+                        <li className='selectionwhite'>Built a strong and loyal customer base, amassing over 1100 orders and 900 5-star reviews. 
+                        Visit <a className='font-medium text-blue-600' href="https://www.fiverr.com/martinbartolo" target="_blank">https://www.fiverr.com/martinbartolo </a> 
+                        to view my work and testimonials.</li>
                     </ul>
                 </div>
             </article>
