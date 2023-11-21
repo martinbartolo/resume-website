443379eb2505271693bdb849777d08e37982fc3e
martin-bartolo <martin9oh9@gmail.com>
2023-11-21 18:57:08 +0100

    Header black background on small screens

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -2,7 +2,7 @@ import Button from "./button";
 
 export default function Nav() {
   return (
-    <div className="fixed flex w-full items-center justify-between p-4">
+    <div className="fixed flex w-full items-center justify-between bg-black p-4 md:bg-transparent">
       <h1 className="cursor-default text-3xl font-bold transition ease-in-out hover:scale-105">
         MB
       </h1>
